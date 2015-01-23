e7282bdd03236129edadd0070e7e47115f2d3b45
ckinne1771 <ckinney1771@gmail.com>
2015-01-23 21:00:09 +0000

    Added Engine Script
    ShipComponent Superclass and EngineScript subclass added. TODO: Add
    other ship components.

---
+++ Assets/EngineScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EngineScript : ShipComponentScript {
+
+	// Use this for initialization
+	void Start () {
+		health = 100;
+		isActive = false;
+		Timer = 3.0f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (isActive ==true)
+		{
+
+			Timer-= Time.deltaTime;
+			if( Timer<=0)
+			{
+				health--;
+				Debug.Log (health);
+				Timer=3.0f;
+			}
+	}
+}
+
+	public void Activate(){
+		Debug.Log ("Working");
+		isActive = true;
+	}
+
+	public void Deactivate(){
+		isActive = false;
+}
+}
---
+++ Assets/ShipComponentScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipComponentScript : MonoBehaviour {
+	
+	
+	public int health;
+	public static bool isActive;
+	public float StartTime;
+	public float Timer;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+		
+	}
+}
