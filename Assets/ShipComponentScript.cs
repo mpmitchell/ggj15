6b575f2ac9725c6ebe04edf11f955eaa60206f0c
Matthew Mitchell <matt.mancer@gmail.com>
2015-01-23 21:52:32 +0000

    Merge branch 'a-branch' of github.com:mpmitchell/ggj15 into CoreComponents
    Conflicts:
    	Assets/EngineScript.cs
    	Assets/MainScene.unity

--- Assets/ShipComponentScript.cs
+++ Assets/ShipComponentScript.cs
@@ -1,23 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ShipComponentScript : MonoBehaviour {
-	
-	
+public class ShipComponentScript : MonoBehaviour
+{	
 	public int health;
 	public static bool isActive;
 	public float StartTime;
 	public float Timer;
-	
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-		
-		
-	}
 }
---
+++ Assets/SlidingSlider.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SlidingSlider : MonoBehaviour
+{
+	public GameObject slider;
+	Slider slider_;
+	EngineScript engine;
+
+	public void Start()
+	{
+		slider_ = slider.GetComponent<Slider>();
+		engine = GetComponent<EngineScript>();
+	}
+
+	public void Update()
+	{
+		if (slider_)
+			slider_.value = engine.health / 100.0f;
+	}
+
+	public void OnSliderChange()
+	{
+		Debug.Log("Slider value: " + engine.health);
+	}
+}
