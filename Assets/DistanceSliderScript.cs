834e75a9dbab05ff10a0134947a9d969b0176ac5
ckinne1771 <ckinney1771@gmail.com>
2015-01-24 06:05:43 +0000

    Victory Condition "added"
    Logic in place. Liam and Mathew will need to implement the scene
    management. TODO: Possible random events, balancing and art
    implementation.

---
+++ Assets/DistanceSliderScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DistanceSliderScript : MonoBehaviour {
+
+	public GameObject slider;
+	Slider slider_;
+	public float distance = 600.0f;
+
+	
+	public void Start()
+	{
+		slider_ = slider.GetComponent<Slider>();
+	}
+	
+	public void Update()
+	{
+		distance -= Time.deltaTime;
+			slider_.value = distance / 600.0f;
+
+		if (distance <= 0) {
+						Debug.Log ("You Win! Liam and Mathew will implement the victory screen");
+				}
+	}
+	
+	public void OnSliderChange()
+	{
+		Debug.Log("Slider value: " + distance);
+	}
+}
