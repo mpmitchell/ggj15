c89de684edf74872833e9f79b0b4affa7fe84d95
ckinne1771 <ckinney1771@gmail.com>
2015-01-24 11:23:59 +0000

    Added Audio. 2 Bugs
    Audio in main scene now. Bug in the fact that audio overshadows another
    playing piece and warning audio glitches at start before playing
    properly. Personally I think that's a feature, adds to the whole
    "falling apart spaceship " theme.

--- Assets/SlidingSlider.cs
+++ Assets/SlidingSlider.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class SlidingSlider : MonoBehaviour
 {
 	public GameObject slider;
+	public GameObject audioSource;
 	public ShipComponentScript shipComponent;
 	Slider slider_;
 
@@ -17,10 +18,23 @@ public class SlidingSlider : MonoBehaviour
 	{
 		if (slider_ && shipComponent)
 			slider_.value = shipComponent.health / 100.0f;
+		if (shipComponent.health <= 30 && shipComponent.health >= 29.5f) {
+						PlayAudio ();
+				}
+						
+	
 	}
 
 	public void OnSliderChange()
 	{
 		Debug.Log("Slider value: " + shipComponent.health);
 	}
+
+	public void PlayAudio()
+	{
+		audioSource = GameObject.Find ("WarningAudio");
+		audioSource.audio.Play ();
+		}
+
 }
+
